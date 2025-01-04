d6c5b12178c52e43ef806c79cc14f29d1af16c7f
dsh410 <dsh410@gmail.com>
2025-01-04 17:25:48 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TaskForm from './components/TaskForm'
+import Search from './components/Search'
+
+function App() {
+
+
+  return (
+    <>
+  
+     <TaskForm />
+
+    </>
+  )
+}
+
+export default App
