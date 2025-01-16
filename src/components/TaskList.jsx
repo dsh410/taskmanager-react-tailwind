3c7567e8b869237604aea2023c7f550c0cef0b86
dsh410 <dsh410@gmail.com>
2025-01-16 05:51:38 -0600

    add todo input form

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,15 @@
 
 import TaskItem from './TaskItem'
 import { useEffect, useState } from 'react';
+
+import TaskInputForm from './TaskInputForm';
 import getTodoItems from '../api';
 
 
 export default function TaskList() {
 
     const [todoItems, setTodoItems] = useState([]);
-    
+
     useEffect(() => {
         async function fetchData() {
             const items = await getTodoItems();
@@ -17,13 +19,17 @@ export default function TaskList() {
     }, []);
 
     return (
+        <>
         <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {todoItems.map((todo) => (
                 <>
                     <TaskItem todo={todo} />
+
                 </>
 
             ))}
         </ul>
+        <TaskInputForm />
+        </>
     )
 }
