10227809bb3fa399e11f9c13bc550ec6e571dc12
dsh410 <dsh410@gmail.com>
2025-01-22 08:12:27 -0600

    remove white space

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -25,7 +25,6 @@ export default function TaskList() {
                     <>
                         <TaskItem todo={todo} />
                     </>
-
                 ))}
             </ul>
             {showAddTodoForm && <TaskInputForm />}
