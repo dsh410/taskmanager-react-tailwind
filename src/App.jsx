535e0b755b377c92f4534b17315a9ad4362bab1d
dsh410 <dsh410@gmail.com>
2025-01-27 07:10:11 -0600

    removed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import Home from './pages/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './pages/Root'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [{
-      index: true,
-      element: <Home />
-    }]
-  },
-])
+
 
 function App() {
   return (
     <>
-      <RouterProvider router={router}>
-        <Home />
-      </RouterProvider>
+      <Home />
     </>
   )
 }
